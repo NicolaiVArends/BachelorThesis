4d24e9591212c88fbd4fe57ce88024c3186f7e0c
NicolaiVArends <nmf376@alumni.ku.dk>
2023-02-10 16:04:52 +0100

    filter asset description

--- src/filter_assets.py
+++ src/filter_assets.py
@@ -1,6 +1,13 @@
 import pandas as pd
 import numpy as np
 
-def filter_asset():
-    
-    return
+def filter_asset(data):
+    """
+    Takes financial asset data and filter it on ESG-score
+    :param data:
+    :returns: Asset data filtered on ESG-score
+    """
+
+
+
+    return 
