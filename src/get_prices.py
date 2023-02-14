826dcdabcdb766707b3664e83fa3ec39b77f42ee
NicolaiVArends <nmf376@alumni.ku.dk>
2023-02-14 15:15:46 +0100

    code skeleton

---
+++ src/get_prices.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+
+def get_financial_data(start_date, end_date, stocks):
+    """
+    
+    :param start_date: The start date for the stock
+    :param end_date:
+    :param stocks: 
+    :returns: A dataframe containing financial returns stock data
+    """
+
+
+    return 
