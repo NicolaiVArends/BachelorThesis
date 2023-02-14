605bce348b718878cba5803f4a15bfbfdd90b5e0
NicolaiVArends <nmf376@alumni.ku.dk>
2023-02-14 15:18:58 +0100

    added skeleton files

---
+++ src/portfolio.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+
+def portfolio_return():
+    return
+
+def portfolio_risk():
+    return
+
+def portfolio_minimize_risk():
+    return
+
+def portfolio_max_sharp_ratio():
+    return
+
