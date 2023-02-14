fdb12990e39a6adc24a7961f66d43d6b10f9626f
NicolaiVArends <nmf376@alumni.ku.dk>
2023-02-14 14:09:04 +0100

    filter universe esg made

--- src/filter_assets.py
+++ src/filter_assets.py
@@ -1,6 +1,15 @@
 import pandas as pd
 import numpy as np
 
-def filter_asset():
-    
-    return
+def filter_universe_esg(esg_score, esg_data: pd.DataFrame):
+    """
+    Takes financial asset data and filter it on ESG-score
+    :param esg_score: Given level of min ESG-score
+    :param esg_data: A Dataframe containing ESG-score for different assets
+    :returns: Asset data filtered on given ESG-score
+    """
+    filtered_df = esg_data[esg_data['esg'] >= esg_score] 
+
+    return filtered_df
+
+
