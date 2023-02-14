11af73009b044224852b44246293dea5f1b35d8b
NicolaiVArends <nmf376@alumni.ku.dk>
2023-02-14 15:18:42 +0100

    added skeleton files for efficient frontier

---
+++ src/efficient_frontier.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+import matplotlib
+
+def efficient_frontier(portfolio_return , portfolio_cov):
+    """
+    
+    :param:
+    :param: 
+    :returns: 
+    """
+
+
+
+    return
+
+
+
+def plot_ef():
+    """
+    
+    :param:
+    :param: 
+    :returns: 
+    """
+
+    return None
