e02615306a3aee13e9a5cbd2fd474c926c0204a8
NicolaiVArends <nmf376@alumni.ku.dk>
2023-02-17 11:33:25 +0100

    computed portfolio risk

--- src/portfolio.py
+++ src/portfolio.py
@@ -4,34 +4,33 @@ from scipy.optimize import minimize
 
 def portfolio_return(weights: pd.DataFrame, returns: pd.DataFrame):
     """
-
-    :param:
-    :param: 
-    :returns: 
+    Function that uses portfolio returns and weights to compute the portfolio return by doing dot product between returns and weights
+    :param: A dataframe of asset returns in portfolio
+    :param: A dataframe or numpy array with the portfolio weights
+    :returns: A float of the computed portfolio risk
     """
     return np.dot(returns, weights)
 
 def portfolio_covariance(returns: pd.DataFrame):
     """
-
-    :param:
-    :param: 
-    :returns: 
+    Function that takes the returns of the different assets in a portfolio and computes the covariance matrix of it
+    :param: A dataframe containing the returns of assets in portfolio
+    :returns: A dataframe of the portfolio covariance matrix
     """
     return returns.cov()
 
 def portfolio_risk(weights: pd.DataFrame, portfolio_covariance):
     """
-    
-    :param:
-    :param: 
-    :returns: 
+    Function that takes portfolio weigths and covariance matrix and computes the portfolio risk
+    :param: A dataframe or numpy array with the portfolio weights
+    :param: A dataframe of the portfolio covariance matrix
+    :returns: A float of the computed portfolio risk
     """
     return np.sqrt(np.dot(weights, np.dot(weights, portfolio_covariance)))
 
-def portfolio_minimize_risk(portfolio_covariance, x0, linear_constraint, bounds, options):
+def portfolio_minimize_risk(portfolio_covariance, esg_score, x0, linear_constraint, bounds, options):
     """
-    Function that will take different inputs and compute the minimum risk of a portfolio
+    Function that will take different inputs and compute the minimum risk of different portfolios u
     :param:
     :param:
     :param:
