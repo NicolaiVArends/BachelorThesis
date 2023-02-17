8c476dff7af8168eb1bdacf575bebedb57141f67
NicolaiVArends <nmf376@alumni.ku.dk>
2023-02-17 09:46:46 +0100

    made portfolio return v1 and added some docu

--- src/portfolio.py
+++ src/portfolio.py
@@ -1,15 +1,42 @@
 import pandas as pd
 import numpy as np
 
-def portfolio_return():
-    return
+def portfolio_return(weight: pd.DataFrame, returns: pd.DataFrame):
+    """
+
+    :param:
+    :param: 
+    :returns: 
+    """
+    return returns.dot(weight)
 
 def portfolio_risk():
+    """
+    
+    :param:
+    :param: 
+    :returns: 
+    """
+
     return
 
 def portfolio_minimize_risk():
+    """
+    
+    :param:
+    :param: 
+    :returns: 
+    """
+
     return
 
 def portfolio_max_sharp_ratio():
+    """
+    
+    :param:
+    :param: 
+    :returns: 
+    """
+ 
     return
 
