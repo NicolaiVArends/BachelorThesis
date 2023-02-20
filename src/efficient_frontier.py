bd2895fe4d90cd84f0ac6807206f804ebe311778
NicolaiVArends <nmf376@alumni.ku.dk>
2023-02-20 11:16:42 +0100

    added function for CML

--- src/efficient_frontier.py
+++ src/efficient_frontier.py
@@ -4,7 +4,7 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
 
-def efficient_frontier(portfolio_return, portfolio_cov):
+def calculate_efficient_frontier(portfolio_return, portfolio_cov):
     """
     
     :param:
@@ -16,9 +16,21 @@ def efficient_frontier(portfolio_return, portfolio_cov):
 
     return
 
+def calculate_capital_market_line():
+    """
+    
+    :param:
+    :param: 
+    :returns: 
+    """
+    
+    slope =
+    cml_x_axis =
+    cml_y_axis =
 
+    return slope, cml_x_axis, cml_y_axis
 
-def plot_efficient_frontier_2D(optimal_portfolio_return, optimal_portfolio_risk, ):
+def plot_efficient_frontier_2D(optimal_sharp_ratio_return, optimal_sharp_ratio_risk, frontier_x_axis, frontier_y_axis):
     """
     
     :param:
@@ -37,8 +49,10 @@ def plot_efficient_frontier_2D(optimal_portfolio_return, optimal_portfolio_risk,
     plt.ylabel('')
     plt.xlim([])
     plt.ylim([])
-    plt.plot(optimal_portfolio_risk, optimal_portfolio_return)
+    plt.plot(optimal_sharp_ratio_risk, optimal_sharp_ratio_return)
+    plt.plot(frontier_x_axis, frontier_y_axis, marker='o')
     plt.legend()
+    plt.show()
 
     return None
 
@@ -55,14 +69,12 @@ def plot_efficient_frontier_3D(optimal_portfolio_return, optimal_portfolio_risk,
     :param: 
     :returns: 
     """
-    mpl.style.use()
-    plt.title('', fontsize=12)
-    plt.xlabel('')
-    plt.ylabel('')
-    plt.xlim([])
-    plt.ylim([])
-    plt.plot(optimal_portfolio_risk, optimal_portfolio_return)
-    plt.legend()
+ 
+    plt.figure()
+    plt.axes(projection ='3d')
+    #plt.plot(x, y, z, cmap ='viridis', edgecolor ='green')
+    plt.title('Surface plot geeks for geeks')
+    plt.show()
 
     return None
 
