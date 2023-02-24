d4c804e33721d148e4abb41008f016a7c2c7eb65
NicolaiVArends <nmf376@alumni.ku.dk>
2023-02-24 11:03:33 +0100

    function to calculate capm

---
+++ src/capm.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+import matplotlib as mpl
+import statsmodels.api as stat
+from src.portfolio import *
+
+def calculate_capital_asset_price_model():
+    """
+    
+    :param:
+    :param:
+    :returns: 
+    """
+    beta = []
+
+    for 
+        x = 
+        y = 
+        endog_var =
+        exog_var =
+
+        model = stat.OLS()
+        result = model.fit()
+
+    return result
