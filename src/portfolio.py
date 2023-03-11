a7ecd8eba8464f0d373694b69339e3c12d218742
NicolaiVArends <nmf376@alumni.ku.dk>
2023-03-11 10:14:31 +0100

    corrected portfolio.py

--- src/portfolio.py
+++ src/portfolio.py
@@ -151,10 +151,12 @@ def plot_cummulative_portfolio_returns(returns: pd.DataFrame,
     """
 
     returns_pct_cumm = returns.pct_change().dropna().cumsum()
+    returns_pct_cumm['PortfolioMean'] = returns_pct_cumm.mean(numeric_only=True, axis=1)
 
     mpl.style.use(mpl_style)
-    for asset in returns_pct_cumm.columns:
-        plt.plot(asset)
+    for asset in returns_pct_cumm:
+        plt.plot(returns_pct_cumm[asset], alpha=0.4)
+    plt.plot(returns_pct_cumm['PortfolioMean'], color='black')
     plt.title(title)
     plt.ylabel("Returns")
     plt.xlabel("Time")
