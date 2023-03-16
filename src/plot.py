4d5ff1951f9887fbfbcece61e78ca114495a6ccd
NicolaiVArends <nmf376@alumni.ku.dk>
2023-03-16 12:36:08 +0100

    moved plots from efficient_frontier.py

--- src/plot.py
+++ src/plot.py
@@ -2,7 +2,6 @@ import pandas as pd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
-
 def plot_cummulative_portfolio_returns(returns: pd.DataFrame,
                            mpl_style='default',
                            title='Portfolio cummulative returns'):
@@ -28,3 +27,145 @@ def plot_cummulative_portfolio_returns(returns: pd.DataFrame,
     plt.show()
 
     return None
+
+def plot_efficient_frontier_return(max_sr_return, 
+                               max_sr_risk, 
+                               frontier_x_axis, 
+                               frontier_y_axis,
+                               cml_x_axis,
+                               cml_y_axis, 
+                               mpl_style='default',
+                               title='Efficient Frontier'):
+    """
+    Function that plot and shows a 2D graph for 
+    :param: 
+    :param:
+    :param:
+    :param: 
+    :param:
+    :param: 
+    :param:
+    :param: 
+    :returns: 
+    """
+    
+    mpl.style.use(mpl_style)
+    plt.title(title)
+    #plt.xlabel('Portfolio Risk')
+    #plt.ylabel('Portfolio Return')
+    plt.xlim([min(frontier_x_axis), max(frontier_x_axis)])
+    plt.ylim([min(frontier_y_axis), max(frontier_y_axis)])
+    plt.plot(frontier_x_axis, frontier_y_axis)
+    plt.plot(max_sr_risk, max_sr_return, marker='o')
+    plt.plot(cml_x_axis, cml_y_axis, label=f'CML')
+    plt.legend()
+    plt.show()
+
+    return None
+
+def plot_efficient_frontier_return_3D(max_sr_return, 
+                               max_sr_risk,
+                               frontier_x_axis, 
+                               frontier_y_axis,
+                               cml_x_axis,
+                               cml_y_axis, 
+                               mpl_style='default',
+                               title='Efficient Frontier 3D'):
+    """
+    
+    :param:
+    :param:
+    :param:
+    :param: 
+    :param:
+    :param: 
+    :param:
+    :param: 
+    :returns: 
+    """
+
+    mpl.style.use(mpl_style)
+    fig = plt.figure(figsize = (10,10))
+    ax = plt.axes(projection='3d')
+    ax.set_title(title)
+    #ax.set_xlabel('Portfolio risk')
+    #ax.set_ylabel('Portfolio returns')
+    #ax.set_zlabel('ESG')
+    ax.scatter(frontier_x_axis, frontier_y_axis, esg, cmap ='viridis')
+    ax.grid()
+    plt.legend()
+    plt.show()
+
+    return None
+
+
+def plot_efficient_frontier_esg_2D(max_sr_return, 
+                               max_sr_risk, 
+                               frontier_x_axis, 
+                               frontier_y_axis,
+                               cml_x_axis,
+                               cml_y_axis, 
+                               mpl_style='default',
+                               title='Efficient Frontier'):
+    """
+    Function that plot and shows a 2D graph for 
+    :param: 
+    :param:
+    :param:
+    :param: 
+    :param:
+    :param: 
+    :param:
+    :param: 
+    :returns: 
+    """
+    
+    mpl.style.use(mpl_style)
+    plt.title(title)
+    #plt.xlabel('Portfolio Risk')
+    #plt.ylabel('Portfolio Return')
+    plt.xlim([min(frontier_x_axis), max(frontier_x_axis)])
+    plt.ylim([min(frontier_y_axis), max(frontier_y_axis)])
+    plt.plot(frontier_x_axis, frontier_y_axis)
+    plt.plot(max_sr_risk, max_sr_return, marker='o')
+    plt.plot(cml_x_axis, cml_y_axis, label=f'CML')
+    plt.legend()
+    plt.show()
+
+    return None
+
+def plot_efficient_frontier_esg_3D(max_sr_return, 
+                               max_sr_risk,
+                               frontier_x_axis, 
+                               frontier_y_axis,
+                               cml_x_axis,
+                               cml_y_axis, 
+                               mpl_style='default',
+                               title='Efficient Frontier 3D'):
+    """
+    
+    :param:
+    :param:
+    :param:
+    :param: 
+    :param:
+    :param: 
+    :param:
+    :param: 
+    :returns: 
+    """
+
+    mpl.style.use(mpl_style)
+    fig = plt.figure(figsize = (10,10))
+    ax = plt.axes(projection='3d')
+    ax.set_title(title)
+    #ax.set_xlabel('Portfolio risk')
+    #ax.set_ylabel('Portfolio returns')
+    #ax.set_zlabel('ESG')
+    ax.scatter(frontier_x_axis, frontier_y_axis, esg, cmap ='viridis')
+    ax.grid()
+    plt.legend()
+    plt.show()
+
+    return None
+
