e3091f55d2ad353296e21f28bea92e6235b328f1
CheeseT99 <95853318+CheeseT99@users.noreply.github.com>
2023-04-16 15:03:18 +0200

    Unit tests

---
+++ src/filter_assets.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+
+
+
+
+
+def filter_asset(data):
+    """
+    Takes financial asset data and filter it on ESG-score
+    :param data:
+    :returns: Asset data filtered on ESG-score
+    """
+
+
+
+    return 
+    
