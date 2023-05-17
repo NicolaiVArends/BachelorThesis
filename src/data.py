f3e5ea24b04467890c169a5a0a5bcf159cd05da9
NicolaiVArends <nmf376@alumni.ku.dk>
2023-05-17 08:53:59 +0200

    fixed typos

--- src/data.py
+++ src/data.py
@@ -12,14 +12,14 @@ def esg_score_weight(data: pd.DataFrame,
                      max_esg_score: int = 2000):
     """ This function takes ESG score data and the wanted weight allocation for environment, sustaniability and governance seperately, calculates and returns ESG data with including the new weighted score.
 
-    In this function, we takes raw ESG score data over the assets in the portfolio from a data provider, a wanted weight allocation for E, S and G seperately, a minimum and maximum acceptabel limit of total wanted weigthed ESG score.
+    In this function, we takes raw ESG score data over the assets from a data provider, a wanted weight allocation for E, S and G seperately, a minimum and maximum acceptabel limit of total wanted weigthed ESG score.
     The function takes the weigthed fractions of E, S and/or G scores from the raw data and make a new column with the total weigthed score wanted. The weight allocation must sum up to 100% = 1.00. Further, the function have a limit setting a minimum or/and maximum level of total ESG score.
 
-    :param data: Raw ESG score for the assets in a portfolio
+    :param data: Raw ESG score for the assets
     :param weights: Weight allocation in percent for environment, sustaniability and governance seperately in an array formated np.array([E, S, G]), must sum to 1.00
     :param min_esg_score: Limit for the minimum acceptable total esg score, default is 500
     :param max_esg_score: Limit for the maximum acceptable total esg score, default is 2000
-    :returns: Raw ESG score including a weighted score according to the wanted levels of environment, sustaniability and governance seperately
+    :returns: Raw ESG score including a new weighted score column according to the wanted levels of environment, sustaniability and governance seperately
     """
     if np.sum(weights) != 1:
         return("Weights must sum to 1")
@@ -37,18 +37,18 @@ def esg_score_weight(data: pd.DataFrame,
             else:
                 pass
         result = data[(data["weighted_score"] >= min_esg_score) & (data["weighted_score"] <= max_esg_score)]
-        
         #data["weighted_score"] <= max_esg_score
         return result
     
+
 def stock_monthly_close(esg_data: pd.DataFrame, 
                         dates: tuple):
-    """ This function uses ESG data in the portfolio and the period, downloads monthly close price data and returns a full dataframe with ESG scores and price data.
+    """ This function uses ESG data for the assets and the period, downloads monthly close price data and returns a full dataframe with ESG scores and price data.
 
     In this function, we take the ESG score and period of the wanted historical price period. With yahoo finance api, we download the monthly close data of each stock from yahoo finance.
     The function then concatenate the dataframes of the ESG score and historical monthly close prices of stocks to one dataframe that is returned.
 
-    :param esg_data: ESG score in portfolio
+    :param esg_data: ESG score for the assets
     :param dates: Period for the historical monthly close price data
     :returns: Dataframe with both monthly close price data for the stocks and esg score
     """
@@ -76,7 +76,7 @@ def stock_monthly_close(esg_data: pd.DataFrame,
 def seperate_full_data(full_data: pd.DataFrame):
     """ This function takes and seperates a full dataframe with both esg weigthed score and returns a tuple with dataframe for prices and ESG scores seperately.
     
-
+    In the function we seperate the esg weighted score columns with the prices/returns columns for the assets data.
 
     :param full_data: Both prices and ESG weigthed scores in one dataframe
     :returns: Prices and ESG scores as seperated dataframes
@@ -98,24 +98,22 @@ def data_for_beta(symbols,
     """
     
     :param symbols: 
-    :param dates:
+    :param dates: Period for the historical data
     :returns:
     """
 
     # create a new dataframe to store the monthly closing data
-    stock_data1 = pd.DataFrame()
+    stock_data = pd.DataFrame()
     for i in range(len(symbols)):
-        stock_data = yf.download(symbols[i], start=dates[0], end=dates[1], interval='1mo', progress=False)
-        #print(stock_data)
-        stock_data = stock_data[['Close']].rename(columns={'Close': symbols[i]})
-       # print(stock_data)
-    # retrieve data from yfinance
-        stock_data1 = pd.concat([stock_data1,stock_data], axis = 1)
-    return stock_data1
+        stock_data_download = yf.download(symbols[i], start=dates[0], end=dates[1], interval='1mo', progress=False)
+        stock_data_download = stock_data_download[['Close']].rename(columns={'Close': symbols[i]})
+        # retrieve data from yfinance
+        stock_data = pd.concat([stock_data,stock_data_download], axis = 1)
+    return stock_data
 
 
 def currency_rates(prices: pd.DataFrame):
-    """
+    """ This function takes the exchange rates from forex-python api for foreign 
     
     :param prices: 
     :returns:
@@ -141,7 +139,6 @@ def currency_rates(prices: pd.DataFrame):
             dates = list(prices.index)
             for month in range(len(prices[asset])):
                 prices[asset][month] = prices[asset][month]/(exchange_rate_dict[dates[month]]["DKK"])
-        
     return prices
 
 
@@ -155,7 +152,7 @@ def pct_returns_from_prices(prices: pd.DataFrame):
     return returns_pct_change
 
 
-def simulate_random_data_esg(n_rows = 10000):
+def simulate_random_data_esg(n_rows: int = 10000):
     """
 
     :param n_rows: 
