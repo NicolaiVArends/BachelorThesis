82787f7cc04461014b138f64b564c8a4d7ce0f9a
NicolaiVArends <nmf376@alumni.ku.dk>
2023-05-17 10:50:25 +0200

    added extra types

--- src/plot.py
+++ src/plot.py
@@ -6,19 +6,18 @@ import ipywidgets
 from mpl_toolkits import mplot3d
 
 def plot_cummulative_portfolio_returns(returns: pd.DataFrame,
-                           mpl_style='default',
-                           title='Portfolio cummulative returns'):
-    """
-    Function that uses return data to plot portfolio returns performance
-    :param: 
-    :param: 
-    :param: 
-    :returns: 
-    """
+                           mpl_style: str = 'default',
+                           title: str = 'Portfolio cummulative returns'):
+    """ This function uses raw price data to plot and show portfolio cummulative returns.
 
+    The function will not return anything but will show a plot when running.
+
+    :param returns: Raw price/returns that is not in percent already
+    :param mpl_style: Matplotlib style of the plot, default is 'default'
+    :param title: Title of the commulative plot, default is 'Portfolio cummulative returns'
+    """
     returns_pct_cumm = returns.pct_change().dropna().cumsum()
     returns_pct_cumm['PortfolioMean'] = returns_pct_cumm.mean(numeric_only=True, axis=1)
-
     mpl.style.use(mpl_style)
     for asset in returns_pct_cumm:
         plt.plot(returns_pct_cumm[asset], alpha=0.4)
@@ -28,30 +27,30 @@ def plot_cummulative_portfolio_returns(returns: pd.DataFrame,
     plt.xlabel("Time")
     plt.legend(returns_pct_cumm)
     plt.show()
-
     return None
 
 
-def plot_efficient_frontier(parameters,start_year,
-                            end_year,
-                            risk_free_rate,
+def plot_efficient_frontier(parameters,
+                            start_year: int,
+                            end_year: int,
+                            risk_free_rate: float,
                             plot_cml = True,
                             plot_max_sharp = False,
                             mpl_style='default',
                             title='Efficient Frontier'):
+    """ This function 
+
+    The function will not return anything but will show a plot of the efficient frontier when running.
+
+    :param parameters:
+    :param start_year:
+    :param end_year:
+    :param risk_free_rate: Risk free rate of investment
+    :param plot_cml: Boolean to enable or disable plotting of capital market line, default is True
+    :param plot_max_sharp: Boolean to enable or disable plotting of max sharp ratio, default is False
+    :param mpl_style: Matplotlib style of the plot, default is 'default'
+    :param title: Title of the commulative plot, default is 'Efficient Frontier'
     """
-    Function that plot and shows a 2D graph for 
-    :param: 
-    :param:
-    :param:
-    :param: 
-    :param:
-    :param: 
-    :param:
-    :param: 
-    :returns: 
-    """
-    
     mpl.style.use(mpl_style)
     plt.xlabel('Risk/Volatility')
     plt.ylabel('Expected Return')
@@ -79,11 +78,6 @@ def plot_efficient_frontier(parameters,start_year,
             plt.plot(cm_x,cm_y, color='k', linewidth = 2, label = 'Capital Market Line')
             plt.plot(opt_sr_vol,  opt_sr_ret, marker='o', color = f'{colors[i]}', markersize=8, label=f'{start_year}-{end_year} Max Sharp Ratio')
             plt.plot(frontier_x, frontier_y, linestyle='--', color = f'{colors[i]}', linewidth=2, label=f'{start_year}-{end_year} Efficient Frontier')
-
-            
-            
-            
-
         else:
             plt.title('Efficient Frontier with Minimum Risk')
             plt.xlim([0.1,0.2])
@@ -93,9 +87,9 @@ def plot_efficient_frontier(parameters,start_year,
             plt.plot(opt_risk_vol,  opt_risk_ret, marker='o', color = f'{colors[i]}', markersize=8, label=f'20{3+i:02d}-20{13+i:02d} Minimum Risk')
             plt.plot(frontier_x, frontier_y, linestyle='--', color = f'{colors[i]}', linewidth=2, label=f'20{3+i:02d}-20{13+i:02d} Efficient Frontier') 
         #plt.plot(frontier_x, frontier_y, linestyle='--', color = f'{colors[i]}', linewidth=2, label=f'20{3+i:02d}-20{13+i:02d} Efficient Frontier') 
+    plt.title(title)
     plt.legend()
     plt.show()
-
     return None
 
 def plot_efficient_frontier_cml(parameters,
@@ -116,37 +110,26 @@ def plot_efficient_frontier_cml(parameters,
     :param: 
     :param:
     :param: 
-    :returns: 
     """
-    
     mpl.style.use(mpl_style)
     plt.xlabel('Risk/Volatility')
     plt.ylabel('Expected Return')
     colors = ['r','b','k','m','g','c', 'lightslategrey', "darkcyan", "purple", "orange", "olive"]
     for i, x in enumerate(parameters):
         opt_sr_vol, opt_sr_ret, opt_risk_vol,  opt_risk_ret, frontier_x, frontier_y, _ = x
-
         if plot_cml == True:
             plt.title('Efficient Frontier with Max Sharp')
             plt.xlim([0.0,0.4])
-            plt.ylim([-0.2,0.7])
-            
+            plt.ylim([-0.2,0.7])      
             cm_x = np.linspace(0,0.5,100)
             cm_y = (risk_free_rate + cm_x*((opt_sr_ret-risk_free_rate)/opt_sr_vol))
             cm_yy = (risk_free_rate + risk*((opt_sr_ret-risk_free_rate)/opt_sr_vol))
-
             if i % 3 != 0:
                 continue
-
             plt.plot(cm_x,cm_y, color='k', linewidth = 2, label = 'Capital Market Line')
             plt.plot(opt_sr_vol,  opt_sr_ret, marker='o', color = f'{colors[i]}', markersize=8, label=f'{start_year:02d}-{end_year:02d} Max Sharp Ratio')
             plt.plot(frontier_x, frontier_y, linestyle='--', color = f'{colors[i]}', linewidth=2, label=f'{start_year:02d}-{end_year:02d} Efficient Frontier')
             plt.plot(risk,cm_yy,  marker="o", markersize=5, markeredgecolor="k", markerfacecolor="k", label = 'wanted return')
-            
-            
-            
-            
-
         else:
             plt.title('Efficient Frontier with Minimum Risk')
             plt.xlim([0.1,0.2])
@@ -156,12 +139,10 @@ def plot_efficient_frontier_cml(parameters,
             plt.plot(opt_risk_vol,  opt_risk_ret, marker='o', color = f'{colors[i]}', markersize=8, label=f'20{3+i:02d}-20{13+i:02d} Minimum Risk')
             plt.plot(frontier_x, frontier_y, linestyle='--', color = f'{colors[i]}', linewidth=2, label=f'20{3+i:02d}-20{13+i:02d} Efficient Frontier') 
         plt.plot(frontier_x, frontier_y, linestyle='--', color = f'{colors[i]}', linewidth=2, label=f'20{3+i:02d}-20{13+i:02d} Efficient Frontier') 
-    
-    #plt.legend()
-    #plt.show()
-    
-
-    #return None
+    plt.title(title)
+    plt.legend()
+    plt.show()
+    return None
 
 def plot_efficient_frontier_return_3D(max_sr_return, 
                                max_sr_risk,
