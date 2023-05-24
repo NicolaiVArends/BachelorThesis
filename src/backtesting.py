2760cc574043537e973d2e2c44d84edc7a284a2e
CheeseT99 <95853318+CheeseT99@users.noreply.github.com>
2023-05-24 22:56:38 +0200

    Almost done with backtesting and developing unittesting for it

--- src/backtesting.py
+++ src/backtesting.py
@@ -42,7 +42,7 @@ def rolling_window_backtesting(prices: pd.DataFrame,
     return backtest_portfolio_return, backtest_portfolio_risk
 
 
-def backtesting(strategy, monthly_or_yearly_rebalancing,rebalancing_freq,start_date,end_date,covariance_window,market_name):
+def backtesting(strategy, monthly_or_yearly_rebalancing,rebalancing_freq,start_date,end_date,covariance_window_yearly,covariance_window_monthly,market_name,benoit_wolfe = True): #,covariance_window_monthly
     """ This function makes a backtest given a strategy, rebalacing informations, information of data period, window size and benchmark market name.
       
     In this function
@@ -56,7 +56,7 @@ def backtesting(strategy, monthly_or_yearly_rebalancing,rebalancing_freq,start_d
     
     assert isinstance(strategy, dict), "strategy should be a dictionary"
     
-    required_keys = ['df', 'weights', 'min_esg_score', 'max_esg_score', 'start_year', 'Bounds1', 'sharpe_type', 'Wanted_return', 'maximum_risk', 'rebalancing_freq', 'risk_free_rate']
+    required_keys = ['df', 'weights', 'min_esg_score', 'max_esg_score', 'start_year', 'bounds', 'sharpe_type', 'wanted_return', 'maximum_risk', 'rebalancing_freq', 'risk_free_rate']
     
     missing_keys = [k for k in required_keys if k not in strategy.keys()]
     
@@ -67,8 +67,9 @@ def backtesting(strategy, monthly_or_yearly_rebalancing,rebalancing_freq,start_d
     assert rebalancing_freq > 0, "rebalancing_freq should be greater than zero"
     assert isinstance(start_date, (str, pd.Timestamp)), "start_date should be a string or a pandas Timestamp object"
     assert isinstance(end_date, (str, pd.Timestamp)), "end_date should be a string or a pandas Timestamp object"
-    assert isinstance(covariance_window, int), "covariance_window should be an integer"
-    assert covariance_window > 0, "covariance_window should be greater than zero"
+    assert isinstance(covariance_window_yearly, int), "covariance_window_yearly should be an integer"
+    assert isinstance(covariance_window_monthly, int), "covariance_window_monthly should be an integer"
+    assert covariance_window_yearly + covariance_window_monthly > 0, "covariance_window should be greater than zero"
     assert isinstance(market_name, str), "market_name should be a string"
 
     if isinstance(start_date, str):
@@ -78,15 +79,21 @@ def backtesting(strategy, monthly_or_yearly_rebalancing,rebalancing_freq,start_d
         end_date = parse(end_date)
 
     assert start_date <= end_date, "start_date should be less than or equal to end_date"
-    covariance_window_time_delta = relativedelta(years=covariance_window) #The time delta for the covariance window
+    covariance_window_time_delta = relativedelta(years=covariance_window_yearly,months=covariance_window_monthly) #The time delta for the covariance window
     esg_data = data.esg_score_weight(strategy['df'],strategy['weights'],strategy['min_esg_score'],strategy['max_esg_score'])
+    #print(esg_data)
     full_data = data.stock_monthly_close(esg_data,[pd.Timestamp(start_date-covariance_window_time_delta),pd.Timestamp(end_date+relativedelta(years=rebalancing_freq,months=1))]) #Making sure we download data from the first covariance date, until the last date we sell our stocks
     prices,esgdata = data.seperate_full_data(full_data)
     pct_returns = data.pct_returns_from_prices(prices)
-    stock_data_download = yf.download(market_name, start=strategy['start_year']-covariance_window_time_delta, end=end_date+relativedelta(years=rebalancing_freq+1), interval='1mo', progress=False) #Downloads the market stock close for same window.
+    if monthly_or_yearly_rebalancing == 'yearly':
+        stock_data_download = yf.download(market_name, start=strategy['start_year']-covariance_window_time_delta, end=end_date+relativedelta(years=rebalancing_freq+1,months=1), interval='1mo', progress=False) #Downloads the market stock close for same window.
+    elif monthly_or_yearly_rebalancing == 'monthly':
+        stock_data_download = yf.download(market_name, start=strategy['start_year']-covariance_window_time_delta, end=end_date+relativedelta(months=rebalancing_freq+1), interval='1mo', progress=False)
+
     stock_data_download = stock_data_download[['Close']].rename(columns={'Close': market_name})
     stock_data = pd.concat([prices,stock_data_download], axis = 1)
     pct = data.pct_returns_from_prices(stock_data)[start_date-covariance_window_time_delta:end_date+relativedelta(years=rebalancing_freq+1)]
+    #print(pct_returns)
     pct.index =pct.index.tz_localize(None)
 
     listparameters = []
@@ -113,18 +120,22 @@ def backtesting(strategy, monthly_or_yearly_rebalancing,rebalancing_freq,start_d
                             start_date,#The end date for portfolio optimization will be the start date minus the covariance window
                             strategy['wanted_return'],
                             strategy['maximum_risk'],
-                            strategy['rebalancing_freq'])) 
+                            strategy['rebalancing_freq'],
+                            benoit_wolfe)) 
+            #print(start_date-covariance_window_time_delta)
             list_of_port_weights.append(efficient_frontier.weights_of_portfolio(prices,listparameters[i]))
+            #print(list_of_port_weights[i])
             list_of_port_esg_scores.append(portfolio.esg_score_of_portfolio(list_of_port_weights[i],esgdata.head(1)))
             list_of_port_allocations.append(portfolio.capital_mark_line_returns(listparameters[i],strategy['risk_free_rate'],strategy['maximum_risk'])[1])
             list_of_cmle_returns.append(portfolio.capital_mark_line_returns(listparameters[i],strategy['risk_free_rate'],strategy['maximum_risk'])[0])
             list_of_cml_allocations.append(portfolio.capital_mark_line_returns(listparameters[i],strategy['risk_free_rate'],strategy['maximum_risk'])[1])
             betas_of_portfolios.append(capm.calculate_portfolio_beta(pct,prices[start_date-covariance_window_time_delta:start_date],list_of_port_weights[i],market_name))            
-            capm_for_portfolio.append(capm.capm_calc(pct[market_name][start_date-covariance_window_time_delta:start_date].tz_localize(None).mean(),strategy['risk_free_rate'],betas_of_portfolios[i]))
+            capm_for_portfolio.append(capm.capm_calc(pct[market_name][start_date-covariance_window_time_delta:start_date-relativedelta(months=1)].tz_localize(None).mean(),strategy['risk_free_rate'],betas_of_portfolios[i]))
             list_of_portfolio_actual_returns.append((portfolio.portfolio_return(prices.loc[start_date+delta],list_of_port_weights[i])
                                                     -portfolio.portfolio_return(prices.loc[start_date],list_of_port_weights[i]))/portfolio.portfolio_return(prices.loc[start_date],list_of_port_weights[i]))
+            #print(start_date+delta+relativedelta(months=1),start_date+relativedelta(months=1))
             list_of_portfolio_actual_returns_cmle.append(list_of_cml_allocations[i][0]*list_of_portfolio_actual_returns[i]+((1-list_of_cml_allocations[i][0])*strategy['risk_free_rate']))
-                                                    
+            #print(prices.loc[start_date+delta],list_of_port_weights[i],prices.loc[start_date])                 #Vi køber den sidste dag i vores optimeringsprofil
                                                     # list_of_cml_allocations[i][0]*strategy['risk_free_rate'])#list of how large a percentage you would have made using your investment strategy
             #List of portfolios actual returns skal laves om så man tager højde cml allokeringen
             list_of_pct_returns_sp500.append((stock_data_download.loc[start_date+delta][0]-stock_data_download.loc[start_date][0])/stock_data_download.loc[start_date][0])#List of how large a percentage you would have made investing everything in the market
@@ -143,18 +154,25 @@ def backtesting(strategy, monthly_or_yearly_rebalancing,rebalancing_freq,start_d
                             start_date,#The end date for portfolio optimization will be the start date minus the covariance window
                             strategy['wanted_return'],
                             strategy['maximum_risk'],
-                            strategy['rebalancing_freq'])) 
+                            strategy['rebalancing_freq'],
+                            benoit_wolfe)) 
+            #print(start_date-covariance_window_time_delta)
             list_of_port_weights.append(efficient_frontier.weights_of_portfolio(prices,listparameters[i]))
+            #print(list_of_port_weights[i])
             list_of_port_esg_scores.append(portfolio.esg_score_of_portfolio(list_of_port_weights[i],esgdata.head(1)))
             list_of_port_allocations.append(portfolio.capital_mark_line_returns(listparameters[i],strategy['risk_free_rate'],strategy['maximum_risk'])[1])
             list_of_cmle_returns.append(portfolio.capital_mark_line_returns(listparameters[i],strategy['risk_free_rate'],strategy['maximum_risk'])[0])
             list_of_cml_allocations.append(portfolio.capital_mark_line_returns(listparameters[i],strategy['risk_free_rate'],strategy['maximum_risk'])[1])
-            betas_of_portfolios.append(capm.calculate_portfolio_beta(pct,prices[start_date-covariance_window_time_delta:start_date],list_of_port_weights[i],market_name))            
+            betas_of_portfolios.append(capm.calculate_portfolio_beta(pct[start_date-covariance_window_time_delta-relativedelta(months=1):(start_date-relativedelta(months=1))],
+                                                                     pct_returns[start_date-covariance_window_time_delta-relativedelta(months=1):(start_date-relativedelta(months=1))],
+                                                                     list_of_port_weights[i],
+                                                                     market_name))            
             capm_for_portfolio.append(capm.capm_calc(pct[market_name][start_date-covariance_window_time_delta:start_date].tz_localize(None).mean(),strategy['risk_free_rate'],betas_of_portfolios[i]))
-            list_of_portfolio_actual_returns.append((portfolio.portfolio_return(prices.loc[start_date+delta]*list_of_port_weights[i])
-                                                    -portfolio.portfolio_return(prices.loc[start_date]*list_of_port_weights[i]))/portfolio.portfolio_return(prices.loc[start_date]*list_of_port_weights[i]))
+            list_of_portfolio_actual_returns.append((portfolio.portfolio_return(prices.loc[start_date+delta],list_of_port_weights[i])
+                                                    -portfolio.portfolio_return(prices.loc[start_date],list_of_port_weights[i]))/portfolio.portfolio_return(prices.loc[start_date],list_of_port_weights[i]))
+            print(prices[start_date-covariance_window_time_delta-relativedelta(months=1):(start_date-relativedelta(months=1))])
             list_of_portfolio_actual_returns_cmle.append(list_of_cml_allocations[i][0]*list_of_portfolio_actual_returns[i]+((1-list_of_cml_allocations[i][0])*strategy['risk_free_rate']))
-                                                    
+            #print(prices.loc[start_date+delta],list_of_port_weights[i],prices.loc[start_date])                 #Vi køber den sidste dag i vores optimeringsprofil
                                                     # list_of_cml_allocations[i][0]*strategy['risk_free_rate'])#list of how large a percentage you would have made using your investment strategy
             #List of portfolios actual returns skal laves om så man tager højde cml allokeringen
             list_of_pct_returns_sp500.append((stock_data_download.loc[start_date+delta][0]-stock_data_download.loc[start_date][0])/stock_data_download.loc[start_date][0])#List of how large a percentage you would have made investing everything in the market
