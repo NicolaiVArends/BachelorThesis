01f6717148568763a67ce25345d0394766fd7ca1
CheeseT99 <95853318+CheeseT99@users.noreply.github.com>
2023-06-11 12:29:42 +0200

    Update capm.py

--- src/capm.py
+++ src/capm.py
@@ -4,27 +4,6 @@ import matplotlib as mpl
 import statsmodels.api as stat
 from src import portfolio
 
-def calculate_portfolio_beta(market: pd.DataFrame, 
-                             portfolio: pd.DataFrame, 
-                             portfolio_weights: pd.DataFrame,
-                             market_name: str):
-    """ This function uses covariance and variance to calculate and return the portfolio betas.
-
-    In this function we takes the price data for a benchmark market, the portfolio returns and the optimal weight allocation for each rolling window of the portfolio.
-    It uses covariance and variance from NumPy to calculate the betas in the model and returns them as a the betas of each portfolio.
-
-    :param market: Price data for benchmark market in the model
-    :param portfolio: Returns data for the portfolio assets
-    :param portfolio_weights: Optimal weight allocations as a dataframe for each calculated rolling window
-    :param market_name: Name of the market you want to calculate betas
-    :returns: Betas for each portfolio 
-    """
-    betas = []
-    for i in range(len(portfolio.columns)):
-        beta = np.cov(np.stack([market[portfolio.columns[i]].to_numpy(),market[market_name].to_numpy()]),ddof=0)[0,1]/np.var(market[market_name])
-        betas.append(beta)
-    beta_of_port = np.sum(np.sum(np.multiply(betas,portfolio_weights)))
-    return (beta_of_port)
 
 def capm_calc(market_expected_returns: pd.DataFrame,
               beta: float,
